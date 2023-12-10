86343ab7f13a68437b75b793406b17b60d33a839
Emmanuel Da Fonseca <emmanuel.dafonseca@gmail.com>
2023-12-10 12:20:05 +0100

    Add Hello-gophers program

---
+++ hello-gophers/go.mod
@@ -0,0 +1,3 @@
+module github.com/edafonseca/go-training/hello-gophers
+
+go 1.21.3
---
+++ hello-gophers/hello-gophers.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/edafonseca/go-training/hello-gophers/greetings"
+)
+
+func main() {
+	fmt.Println(greetings.Hello())
+}
