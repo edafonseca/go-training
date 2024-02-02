19bee52cbeabd223094bac5b9b5efe8679b3fef1
Emmanuel Da Fonseca <emmanuel.dafonseca@gmail.com>
2024-02-02 11:20:58 +0100

    Return values

--- functions/control/control.go
+++ functions/control/control.go
@@ -2,6 +2,31 @@ package control
 
 import "fmt"
 
+var (
+	x = 0
+	y = 0
+)
+
 func MoveForward(distance, speed int) {
+	fmt.Printf("Adresse mémoire dans MoveForward : %v\n", &distance)
+	fmt.Printf("Valeur avant modification : %v\n", distance)
+
+	distance = 10
+
+	fmt.Printf("Valeur après modification : %v\n", distance)
+
 	fmt.Println("Move forward", distance, speed)
+
+	x, y = calculateNextPosition(x, y, distance, 0)
+}
+
+func Position() (int, int) {
+	return x, y
+}
+
+func calculateNextPosition(x, y, distanceX, distanceY int) (newX, newY int) {
+	newX = x + distanceX
+	newY = y + distanceY
+
+	return
 }
--- functions/main.go
+++ functions/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/edafonseca/go-training/functions/control"
+import (
+	"fmt"
+
+	"github.com/edafonseca/go-training/functions/control"
+)
 
 func main() {
-	control.MoveForward(5, 10)
+	d := 5
+
+	fmt.Printf("Adresse mémoire : %v, type de base : %T, type de l'opérateur d'adresse : %T \n", &d, d, &d)
+
+	control.MoveForward(d, 10)
+	fmt.Printf("Valeur dans la fonction main : %v\n", d)
+
+	x, y := control.Position()
+
+	fmt.Printf("Nouvelle position du robot, x: %v, y: %v\n", x, y)
+	control.MoveForward(10, 1)
+
+	x, y = control.Position()
+	fmt.Printf("Nouvelle position du robot, x: %v, y: %v\n", x, y)
 }
